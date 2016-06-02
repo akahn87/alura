84848204a763bee7cae24e921a357e7917c3bec6
Andy Kahn <andy.kahn@teamaol.com>
2016-06-02 23:49:50 +0200

    Readme updates and tooling

--- src/javascripts/behaviors/analytics.js
+++ src/javascripts/behaviors/analytics.js
@@ -3,11 +3,11 @@ export default function(context) {
 	var analyticsSrvc;
 
 	return {
-		onclick: function(event, element, elementType){
-			analyticsSrvc.userClicked('something');
-		},
 		init: function(){
 			analyticsSrvc = context.getService('analytics');
+		},
+		onclick: function(event, element, elementType){
+			analyticsSrvc.userClicked('something');
 		}
 	}
 }
